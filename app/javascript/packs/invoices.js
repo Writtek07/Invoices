d59716030c7646cb16faa68a76adce5d401ef72c
Writtek <writtek@weinvests-MacBook-Pro.local>
2022-10-13 23:49:12 +0530

    Updated Delete button, Javascript referencing, added Themify as assest to rails

--- app/javascript/packs/invoices.js
+++ app/javascript/packs/invoices.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 
   $('#addrow').click(function(){
-    $('.item-row:last').after('<tr class="item-row"><td><input class="form-control item_desc"/></td><td><input class="form-control item_desc"/></td></tr>');
+    $('.item-row:last').after('<tr class="item-row"><td><input class="form-control item_desc"/></td><td><input class="form-control cost" placeholder="00.00" /></td><td class="delete_td"><a class="delete" href="javascript:;" title="Remove row">Remove <span class="ti-trash"></span></a></td></tr>');
     if ($('.delete').length > 0) $('.delete').show();
   });
 
   $('body').on('click','.delete',function(){
-    &(this).parents.('.item-row').remove();
+    $(this).parents('.item-row').remove();
 
     if ($('.delete').length < 2) $('.delete').hide();
   });
